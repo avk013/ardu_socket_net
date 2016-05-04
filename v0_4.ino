980135c74b8a9486ee89361862918a3f12880bf4
Ali Koba <avk013@gmail.com>
2016-05-04 21:09:41 +0300

    Update v0_4.ino

--- v0_4.ino
+++ v0_4.ino
@@ -4,30 +4,20 @@ Web client+
  * Ethernet shield attached to pins 10, 11, 12, 13
 created 18 Dec 2009 by David A. Mellis
 modified 9 Apr 2012 by Tom Igoe, based on work by Adrian McEwen
-modified 4 May 2019 by Alexander Kovalenko
+modified 4 May 2016 by Alexander Kovalenko
  */
 
 #include <SPI.h>
 #include <TimerOne.h>
 #include <UIPEthernet.h>
 
-// Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
 byte mac[] = { 0x13, 0x13, 0x13, 0x13, 0x13, 0x13 };
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-//char server[] = "www.google.com";    // name address for Google (using DNS)
-//char server[] = "www.timeapi.org";
+
 char server[] = "edis.pp.ua";
-// Set the static IP address to use if the DHCP fails to assign
 IPAddress ip(192,168,200,89);
 IPAddress gw(192,168,200,223);
 IPAddress dnsr(8,8,8,8);
 
-// Initialize the Ethernet client library
-// with the IP address and port of the server 
-// that you want to connect to (port 80 is default for HTTP):
 int i=0;
 EthernetClient client;
 void setup() {
@@ -59,35 +49,22 @@ delay(1000);
     client.println();
   } 
   else {
-    // kf you didn't get a connection to the server:
     Serial.println("connection failed");
   } 
 i=0;
-//  Timer1.restart();
 }
 void tmr()
-{ 
-  ++i;
+{ ++i;
 Serial.print(".");
-if (i==30) {i=0; 
-//    if (!client.connected()) {
-//    client.stop();
-//readnet();
-//}
-//Serial.println();
+if (i==30) {i=0;}
 }
-}
-
-
 
 void loop()
 {
       char c;
   if (client.available()) {
     c = client.read();
-//    Serial.print(c);
 }
   if(c=='$'){c = client.read(); if(c=='1') Serial.println("OK+");else Serial.println("OK-");}
-  //  if (!client.connected()) {client.stop();}
   if (i>=25) readnet();
 }
